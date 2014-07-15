66327dbc1a5d7c179df40e65709e3198798a728f
Michael Kaminsky <mkaminsky11@gmail.com>
2014-07-15 15:11:44 -0400

    users + xkcd

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 ##Harvard P3##
+###Michael Kaminsky###
+Built with <a href="http://polymer-project.org">Polymer</a>, Faker (for generating users) and Badcow (Lorem Ipsum).
+
+Hosed with OpenShift at <a href="http://php-harvardp3.rhcloud.com/">http://php-harvardp3.rhcloud.com/</a>
+
+Consists of:
+<ol>
+	<li>Lorem Ipsum generator</li>
+	<li>XKCD password generator</li>
+	<li>Random user generator</li>
+</ol>
+
+The password generator and user generator and customizable
--- app/views/user.blade.php
+++ app/views/user.blade.php
@@ -1,10 +1,39 @@
 @extends('_master')
 
+
 @section('title')
-User Generator
+Lorem Generator
+@stop
+
+@section('scripts')
+<?php
+include('user.php')
+?>
 @stop
 
 
 @section('content')
-<p>users</p>
+<form method="POST" action="http://php-harvardp3.rhcloud.com/user">
+
+  <h1>USER GENERATORS</h1>
+  <h4>Pick a number of users</h4>
+
+  <br/>
+  <input type="text" value="10" class="dial" name="dial" data-min="0" data-max="50" width="400px" style="margin-left:calc(50% - 200px)">
+  <br/>
+
+
+  <input type="submit" class="submit" value="Generate!">
+
+  <div id="out">
+echo($output);
+  </div>
+</form>
+
+<script src="knob.js"></script>
+<script>
+$(function() {
+    $(".dial").knob();
+});
+</script>
 @stop
